423462be7f38fd6d767c88d14d173447cdd1d081
Christian Buggle <christian@buggle.net>
2018-03-13 14:00:14 +0100

    Initial commit
    * rails new kvv-live -C -M -O -S --webpack=react
    * Add gem capybara-screenshot, launchy, simplecov
    * ruby 2.5.0

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :headless_chrome
+end
+
+ActionDispatch::SystemTesting::Server.silence_puma = true
